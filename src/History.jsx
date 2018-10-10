0e706b2157174879275e0561db5051833ddc804e
Arlensiu Celis <arlensiu.celis@gmail.com>
2018-10-10 23:51:17 +0200

    Quest to include route dynamically.

---
+++ src/History.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div className="container justify-content-center d-flex py-3">
+      Notre histoire
+    </div>
+  );
+};
+
+export default History;
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container justify-content-center d-flex py-3">
+      Bienvenue sur l'accueil
+    </div>
+  );
+};
+
+export default Home;
